2d0f02ea6cf7f4ad67b132198c8c7e3cfcf92bca
Durkgame <medrenguard@gmail.com>
2017-12-11 07:35:14 +0400

    added unzip-function for BigData across GET

--- main.js
+++ main.js
@@ -6,7 +6,22 @@ document.getElementById('cloud-accurate').onclick = function() {
 	document.getElementById('b-accurate').classList.remove('hide');
 	document.getElementById('b-relative').classList.add('hide');
 };
-var mass = JSON.parse(decodeURI(location.search.slice(1)));
+var mass = [];
+var get = unzipRequest(location.search.slice(1));
+function unzipRequest(str) {
+	function unzipPhrase(phrase) {
+		if (phrase.search('_') >= 0) {
+			phrase = phrase.replace(/_/g, ' ');
+		};
+		return phrase;
+	};
+	str = str.split('|');
+	for (var i = 0; i < str.length; i++) {
+		str[i] = str[i].split(':');
+		mass.push({text: unzipPhrase(str[i][0]), weight: Number(str[i][1])});
+	};
+	return str;
+};
 $(function () {
 	$("#b-relative").jQCloud(mass, {method:'relative'});
 });
